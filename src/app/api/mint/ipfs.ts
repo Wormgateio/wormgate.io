c42af52182a348d81c92ef14b51dddc04787544a
Konstantin Teryohin <konstantin.teryohin@rubius.com>
2024-02-24 17:54:45 +0700

    added some nfts

--- src/app/api/mint/ipfs.ts
+++ src/app/api/mint/ipfs.ts
@@ -23,5 +23,35 @@ export const ipfs = [
         name: 'NFT',
         hash: 'QmRsJcJMmXqH3aPYKRX8XRkbtjFEgDhx3krDFEv18Sy3Wf',
         fileName: 'db3c184227144cf91166da4c9380d3ba.png'
-    }
+    },
+    {
+        name: 'Enchanted Cloak',
+        hash: 'QmPpnMoNP863Eh6zPSP6zW4LSqCYLUeKpFRTMGBdBpgvKo',
+        fileName: 'photo_2024-02-23_19-12-30.jpg'
+    },
+    {
+        name: 'Majestic Axe',
+        hash: 'QmZagreYg2RUUkbeunyphCwwnyxbsXSQDhpEJ8Yc1CvfNJ',
+        fileName: 'photo_2024-02-23_19-46-50.jpg'
+    },
+    {
+        name: 'Celestial Bow',
+        hash: 'QmP5pgXS9wcz6vSJsb8ADBDgjuPt3nqvs4ANN11oUBTefe',
+        fileName: 'photo_2024-02-23_19-52-34.jpg'
+    },
+    {
+        name: 'Mystical Timepiece',
+        hash: 'QmRyuSvTZKRKH65L9qHUXGAen9R64RqGarvHSvjTMQZKJz',
+        fileName: 'photo_2024-02-23_19-54-26.jpg'
+    },
+    {
+        name: 'Сelestial Сompass',
+        hash: 'QmWbgR4o4EjDFLiFo7B93fUWjBoVLzyCce35EwaaiXiSHu',
+        fileName: 'photo_2024-02-23_19-57-53.jpg'
+    },
+    {
+        name: 'Galactic Aegis',
+        hash: 'QmcYrnKd1zxCLtNFFz2yNguPgdHEZwnEqCeFtSfBDkr17E',
+        fileName: 'photo_2024-02-23_20-06-46.jpg'
+    },
 ];
